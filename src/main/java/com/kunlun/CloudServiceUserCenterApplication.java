54ff3c70c6acb23b740e0300e7eded2ecc0f8d8d
Jarven <769558579@qq.com>
2017-12-21 10:36:29 +0800

    个人中心

---
+++ src/main/java/com/kunlun/CloudServiceUserCenterApplication.java
@@ -0,0 +1,17 @@
+package com.kunlun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+public class CloudServiceUserCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceUserCenterApplication.class, args);
+	}
+}
